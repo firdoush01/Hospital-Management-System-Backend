1520024bcfc160df4a7c83f6969b416802521866
firdoush <mdfirdoushali1776@gmail.com>
2024-11-05 19:28:21 +0530

    created user Schema

---
+++ backend/models/userSchema.js
@@ -0,0 +1,79 @@
+import mongoose from "mongoose";
+import validator from "validator";
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken";
+
+const userSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true,
+    },
+    lastName: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        validate: {
+            validator: (value) => validator.isEmail(value),
+            message: "Provide a valid email"
+        }
+    },
+    phone: {
+        type: String,
+        required: true,
+    },
+    nic: {
+        type: String,
+        required: true,
+    },
+    dob: {
+        type: Date,
+        required: true,
+    },
+    gender: {
+        type: String,
+        required: true,
+        enum: ["Male","Female","Others"]
+    },
+    password: {
+        type: String,
+        required: true,
+        select: false
+    },
+    role: {
+        type: String,
+        required: true,
+        enum: ["Admin","Patient","Doctor"]
+    },
+    doctorDepartment: {
+        type: String,
+    },
+    docAvatar: {
+        public_id: String,
+        url: String
+    },
+});
+
+// hash password when user registers
+userSchema.pre("save", async function(next){
+    if(!this.isModified("password")){
+        next()
+    }
+    this.password = await bcrypt.hash(this.password,10)
+})
+
+// password matching while login
+userSchema.methods.comparePassword = async function(enteredPassword){
+    return await bcrypt.compare(enteredPassword, this.password)
+}
+
+// 
+userSchema.methods.generateJsonWebToken = function(){
+    return jwt.sign({id: this._id}, process.env.JWT_SECRET_KEY,{
+        expiresIn: process.env.JWT_EXPIRES,
+    })
+}
+
+export const User = mongoose.model("User", userSchema);
