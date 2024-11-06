e5a87df45532d5fddb89e12a6ae8e704dcb6f68c
firdoush <mdfirdoushali1776@gmail.com>
2024-11-06 16:23:09 +0530

    authentication

--- backend/router/userRouter.js
+++ backend/router/userRouter.js
@@ -1,13 +1,14 @@
 import express from "express";
 import { addNewAdmin, patientRegister } from "../controllers/userController.js";
 import { login } from "../controllers/userController.js";
+import { isAdminAuthenticated } from "../middlewares/auth.js";
 
 
 const router = express.Router();
 
 router.post("/patient/register", patientRegister);
 router.post("/login", login);
-router.post("/admin/addnew", addNewAdmin);
+router.post("/admin/addnew", isAdminAuthenticated,addNewAdmin);
 
 
 export default router;
