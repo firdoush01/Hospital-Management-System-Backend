a06534508aaa32ab0edcd3b220999c64e1791a23
firdoush <mdfirdoushali1776@gmail.com>
2024-11-11 23:57:11 +0530

    added frontend and dashboard url

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ import appointmentRouter from "./router/appointmentRouter.js"
 
 app.use(
     cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"], 
+    origin:  [process.env.FRONTEND_URL, process.env.DASHBOARD_URL], 
     methods: ["GET","POST","PUT","DELETE"],
     credentials: true
     })
